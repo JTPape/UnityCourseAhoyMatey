7c84aba815ef2d14fa84c79ac62d3819ade5336f
Joan Tomas Pape <joanpape@gmx.de>
2016-04-02 12:59:00 +0200

    Controls!

---
+++ Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+
+	private Vector3 inputValue;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		inputValue.x = CrossPlatformInputManager.GetAxis ("Horizontal");
+		inputValue.z = CrossPlatformInputManager.GetAxis ("Vertical");
+		inputValue.y = 0f;
+
+		transform.Translate (inputValue);
+	}
+
+
+}

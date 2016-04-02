68f62531eca84e8e9552e3b922006b03ab625384
Joan Tomas Pape <joanpape@gmx.de>
2016-04-02 11:54:39 +0200

    Basic Networking to test

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public void MyStartHost ()
+	{
+		Debug.Log ("" + Time.timeSinceLevelLoad + " Host Starting...");
+		StartHost ();
+	}
+
+
+	public override void OnStartHost ()
+	{
+		
+		Debug.Log ("" + Time.timeSinceLevelLoad + " Host Started Successfully");
+	}
+
+	public override void OnStartClient (NetworkClient myClient)
+	{
+		Debug.Log ("" + Time.timeSinceLevelLoad + " Client " + myClient + " requested start");
+	}
+
+	public override void OnClientConnect (NetworkConnection conn)
+	{
+
+		Debug.Log ("" + Time.timeSinceLevelLoad + " Client connected to " + conn.address);
+//		base.OnClientConnect (conn);
+	}
+}
